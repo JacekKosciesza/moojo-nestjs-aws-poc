dcd925ae6c8aff26d4ac1492d751dbb4119647a5
Jacek Kościesza <jacek.kosciesza@gmail.com>
2023-03-29 14:37:21 +0200

    Prisma recipe

--- moojo/src/main.ts
+++ moojo/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 import { expressApp } from './express';
 
 async function bootstrap() {
   // https://stackoverflow.com/a/68943920
   const adapter = new ExpressAdapter(expressApp);
   const app = await NestFactory.create(AppModule, adapter);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(3000);
 }
